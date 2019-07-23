63912729ddfbc6aa4c5ddf2c89655b2067c5fc5e
kathleensheldon <kcsheldon@verizon.net>
2019-07-23 15:08:41 -0400

    Added a new page with form to save fruit/vegetable input to the database. Added appropriate routing in the ProduceController. Added a link to the index page to go to the new input form.

--- src/main/java/com/launchacademy/fruitveg/controllers/ProduceController.java
+++ src/main/java/com/launchacademy/fruitveg/controllers/ProduceController.java
@@ -1,10 +1,17 @@
 package com.launchacademy.fruitveg.controllers;
 
+import com.launchacademy.fruitveg.models.Produce;
 import com.launchacademy.fruitveg.repositories.ProduceRepository;
+import javax.validation.Valid;
+import org.aspectj.weaver.patterns.TypePatternQuestions;
+import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +27,21 @@ public class ProduceController {
   public String getIndex(Model model) {
     model.addAttribute("produce", produceRepository.findAll());
     return "produce/index";
+  }
+  @GetMapping("/new")
+  public String getNewForm(@ModelAttribute Produce produce, Model model) {
+    model.addAttribute("produce", produce);
+    return "produce/new";
+  }
+  @PostMapping("/new")
+  public String create(@ModelAttribute @Valid Produce produce, BindingResult binding,
+      Model model) {
+
+    if (binding.hasErrors()) {
+      return "produce/new";
+    } else {
+      produceRepository.save(produce);
+      return "redirect:/index";
+    }
   }
 }
